3295a7a81c787ec0a223e95870fea22c8aab9046
tylartylartylar <tylar@tylar.today>
2025-02-20 05:48:24 -0600

    Created player rotation on key presses

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
                         if event.type == pygame.QUIT:
                                 return
                 screen.fill((0,0,0))
+                player.update(dt)
                 player.draw(screen)
                 pygame.display.flip()
                 dt = timeClock.tick(60) / 1000
