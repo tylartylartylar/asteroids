4b85b418d075d059f2d1bbef775d55380fcc2425
tylartylartylar <tylar@tylar.today>
2025-02-19 18:49:05 -0600

    Add FPS control and delta time tracking

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ print(f"Screen height: {SCREEN_HEIGHT}")
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+	timeClock = pygame.time.Clock()
+	dt = 0
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
@@ -16,6 +17,7 @@ def main():
 
 		screen.fill((0,0,0))
 		pygame.display.flip()
+		dt = timeClock.tick(60) / 1000
 
 
 
